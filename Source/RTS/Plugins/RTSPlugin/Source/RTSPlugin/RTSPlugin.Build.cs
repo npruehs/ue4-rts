308d21898c95912ad8723605492df4d1cbe1b8cc
Nick Prühs <dev@npruehs.de>
2017-12-16 14:26:50 +0100

    Add Landscape module to build dependencies.

--- Source/RTS/Plugins/RTSPlugin/Source/RTSPlugin/RTSPlugin.Build.cs
+++ Source/RTS/Plugins/RTSPlugin/Source/RTSPlugin/RTSPlugin.Build.cs
@@ -23,7 +23,8 @@ namespace UnrealBuildTool.Rules
                     "Slate",
                     "SlateCore",
                     "RHI",
-                    "RenderCore"
+                    "RenderCore",
+                    "Landscape"
                 });
 		}
 	}
