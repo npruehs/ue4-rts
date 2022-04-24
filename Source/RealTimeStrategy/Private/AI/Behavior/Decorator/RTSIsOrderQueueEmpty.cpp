f6a733583509c430d1495dd6e1e407c958eaff6e
Mephiztopheles <m.ahrweiler@acinex.com>
2022-04-24 13:21:15 +0200

    Only Issue Stop when queue is empty

---
+++ Source/RealTimeStrategy/Private/AI/Behavior/Decorator/RTSIsOrderQueueEmpty.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Behavior/Decorator/RTSIsOrderQueueEmpty.h"
+
+#include "RTSPawnAIController.h"
+#include "Libraries/RTSFunctionLibrary.h"
+
+URTSIsOrderQueueEmpty::URTSIsOrderQueueEmpty(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+	NodeName = "Is Order Queue Empty";
+}
+
+bool URTSIsOrderQueueEmpty::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const ARTSPawnAIController* Controller = Cast<ARTSPawnAIController>(OwnerComp.GetOwner());
+	if (!IsValid(Controller))
+	{
+		return false;
+	}
+
+	return !Controller->HasOrderQueue();
+}
+
+FString URTSIsOrderQueueEmpty::GetStaticDescription() const
+{
+	return URTSFunctionLibrary::CreateDecoratorDescription(NodeName, {}, GetFlowAbortMode(), IsInversed());
+}
